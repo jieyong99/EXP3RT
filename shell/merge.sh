0198332c00fb7ddfe7b867232cdff32e74cd39fe
Kim Jieyong <65717793+jieyong99@users.noreply.github.com>
2024-08-22 07:21:19 +0900

    Update merge.sh

--- shell/merge.sh
+++ shell/merge.sh
@@ -2,7 +2,7 @@
 
 python -u merge_llama.py
 
-# 설정 배열 정의
+
 declare -a configs=(
   # "64 16 2 42"
   # "64 16 4 42"
@@ -16,14 +16,13 @@ declare -a configs=(
   # "256 16 6 42"
 )
 
-# 각 설정에 대해 반복
+
 for config in "${configs[@]}"; do
-  # 공백을 기준으로 설정 분리
+
   read -r r alpha epoch seed <<< "$config"
   
   echo "Processing: r=$r, alpha=$alpha, epoch=$epoch, seed=$seed"
   
-  # Python 스크립트 실행
   python -u merge_llama.py $r $alpha $epoch $seed
   
   echo "--------------------"
